7f4c5609300193467a016066f0b78a8aeba75688
Mohamed Hajlaoui <mouhamed.hajlaoui@gmail.com>
2021-02-07 13:06:56 +0100

    Release 2.5.5

--- Package.swift
+++ Package.swift
@@ -17,15 +17,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/DataDome/datadome-ios-package", from: "2.5.5")
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.0.0")
+        .package(url: "https://github.com/DataDome/datadome-ios-package.git", from: "2.5.5")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.0.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataDomeAlamofire",
-            path: "Source"
+            path: "Source",
+            dependencies: ["Alamofire", "DataDomeSDK"]
         )
     ],
     swiftLanguageVersions: [.v5]
