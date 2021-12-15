23c92d8b4e4ea3c6dc76e703b87417694de17979
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2021-12-16 00:30:09 +1100

    Add DataDomeSDK dependecy

--- Package.swift
+++ Package.swift
@@ -17,14 +17,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "2.7.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataDomeAlamofire",
-            dependencies: ["Alamofire"],
+            dependencies: ["Alamofire",
+                           "DataDomeSDK"],
             path: "Sources"
         )
     ],
