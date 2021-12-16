cc1108c043a6f6c231dfa0998ab8f710e56899b7
Carlos Monzon <carlosmonzonsalvador@gmail.com>
2021-12-16 15:24:36 +1100

    Update Alamofire dependency

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DataDomeAlamofire",
     platforms: [
-        .iOS(.v12),
+        .iOS(.v13),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.4.4"),
         .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "2.7.0")
     ],
     targets: [
