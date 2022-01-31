3870ec35ea30c84f029188ad1645d475a703ce93
Mohamed Hajlaoui <mohamed.hajlaoui@datadome.co>
2022-01-31 12:25:11 +0100

    Release 2.7.9

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0")
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
         .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "2.7.9")
     ],
     targets: [
