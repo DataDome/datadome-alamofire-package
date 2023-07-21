8851c6b671a036657d5bb4c9eb5ee6d88cca13bd
Kireyin <kireyin@icloud.com>
2023-07-21 13:47:19 +0200

    Update version to 3.2.1 (#6)

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DataDomeAlamofire",
     platforms: [
-        .iOS(.v10),
+        .iOS(.v11),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "3.1.0")
+        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: "3.2.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
