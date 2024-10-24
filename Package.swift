fbb23476a9664d184137f2f49298239308929ee6
Alexandre Brispot <alexandre.brispot@datadome.co>
2024-10-24 18:21:15 +0200

    Release 3.6.1

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 0))
+        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 1))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
