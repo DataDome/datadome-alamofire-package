874caf3f67dd15860d1f2cd2f1eef55442c1d04a
Kireyin <alexandre.brispot@datadome.co>
2024-11-13 16:06:02 +0100

    Release 3.6.2 (#24)
    * Set dependency to beta version of DD_SDK
    
    * Switch dependency to stable version of DD_SDK

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.3
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,20 +8,16 @@ let package = Package(
         .iOS(.v11),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DataDomeAlamofire",
             targets: ["DataDomeAlamofire"]
         ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.0.0"),
-        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 1))
+        .package(name: "DataDomeSDK", url: "https://github.com/DataDome/datadome-ios-package", from: Version(3, 6, 2))
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DataDomeAlamofire",
             dependencies: ["Alamofire", "DataDomeSDK"],
